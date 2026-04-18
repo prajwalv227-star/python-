1f53a06424fcbc03924a4ce029c3960ff00d1a43
Prajwal v <prajwalv227@gmail.com>
2026-04-18 20:31:29 +0530

    add simple calculator using conditional statements
    Implement basic calculator functionality with user input for addition, subtraction, multiplication, and division.

---
+++ Conditional_statement/calculator.py
@@ -0,0 +1,14 @@
+a= int(input("enter first number:"))
+b=int (input("enter second number:"))
+print ("1. add 2.subtract 3.multipy 4.divide")
+choice =int (input ("enter choice :"))
+if choice ==1:
+print ("result:",a+b)
+elif choice ==2:
+print ("result :" a-b)
+elif choice ==3:
+print ("result :" a*b)
+elif choice ==4:
+print ("result :" a/b)
+else :
+print ("invalid choice") 
